dcf02a528567843317528b1cdf38edb72a0372c9
sven van straalen <sven.van.straalen@capgemini.com>
2021-12-01 20:30:02 +0100

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+
+
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
   //eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/cms');
+  //eleventyConfig.setLibrary('md', markdownLib)
   return {
     dir: {
       input: 'src',
--- src/_scripts/slides.js
+++ src/_scripts/slides.js
@@ -2,6 +2,7 @@ import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import revealStyles from 'reveal.js/dist/reveal.css'
 import revealThemeBlack from 'reveal.js/dist/theme/black.css'
+import slidesCustom from '../css/slidesCustom.css'
 
 let deck = new Reveal({
    plugins: [ Markdown ]
