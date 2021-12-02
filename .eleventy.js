9b3c037ddbe068989f10ed959a59c279f5608fd4
sven van straalen <sven.van.straalen@capgemini.com>
2021-12-02 10:50:37 +0100

    added stuff

--- .eleventy.js
+++ .eleventy.js
@@ -11,12 +11,14 @@ const markdownItOptions = {
 const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
 
 module.exports = function (eleventyConfig) {
+  console.log(process.env.ELEVENTY_ENV)
   eleventyConfig.addPassthroughCopy('src/css');
   //eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/cms');
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.setLibrary('md', markdownLib)
   return {
+    pathPrefix: process.env.ELEVENTY_ENV === 'production' ? 'PROTO-netlify-cms' : '',
     dir: {
       input: 'src',
       output: 'dist',
